affabb79bd749bdf3558df11808ca9c57552a479
Williams-TWLK <127419079+Williams-thewhileloopking@users.noreply.github.com>
2023-10-06 19:13:41 +0000

    Checking the testPythonFile.py
    Wonder where this will show

--- testPythonFile.py
+++ testPythonFile.py
@@ -2,7 +2,7 @@ def main():
     print(this_works())
 
 
-def this_works():
+def this_works():  # Day 2: just checking in, everything seems fine.
     return "this works"
 
 if __name__ == "__main__":
